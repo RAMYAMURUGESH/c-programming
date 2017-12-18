246e1699e7f189780ead02198f46f4335692e74b
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2017-12-18 08:48:21 +0000

    Create variables1.c

---
+++ Beginner Level/variables1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char ch1[50]="Sachin Ramesh Tendulkar";
+char ch2[50]="Little Master";
+int score=18426;
+float f=44.83;
+int matches=463;
+printf("%s\n",ch1);
+printf("%s\n",ch2);
+printf("%d\n",score);
+printf("%f\n",f);
+printf("%d\n",matches);
+}
