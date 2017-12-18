0b3c781f0eedccf14b1d343b68f9dba67e898468
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2017-12-18 11:03:41 +0000

    Create scope.c

---
+++ Beginner Level/scope.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int count=100;
+void main()
+{
+int count=50;
+{
+int count=10;
+printf("%d\n",count);
+}
+printf("%d\n",count);
+func();
+}
+void func()
+{
+printf("%d\n",count);
+}
