7ba9f31077bd324b11f4e09c23e919184e3919e1
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-01-07 11:32:22 +0530

    Create macro.c

---
+++ Pro level/macro.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+main(){ 
+ printf("File :%s", __FILE__ ); 
+ printf("Date :%s", __DATE__ ); 
+ printf("Time :%s", __TIME__ ); 
+ printf("Line :%d", __LINE__ ); 
+ printf("ANSI :%d", __STDC__ );
+}
